e54b027dd8468d0cbd63e806a28dfd5790094ef8
Maraba23 <78489243+Maraba23@users.noreply.github.com>
2021-11-01 19:01:43 -0300

    botao do capeta

---
+++ projeto1/js/js paginas condicionais/redirect.js
@@ -0,0 +1,20 @@
+function Formulario() {
+    window.location.replace("paginas/pagina2.html");
+}
+
+function Dropbox() {
+    document.getElementById("Dropbox").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+    var openDropdown = dropdowns[i];
+    if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
