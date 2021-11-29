7d53395718429eae9fe341fa7be3faa3e3c367bd
Maraba23 <78489243+Maraba23@users.noreply.github.com>
2021-11-29 16:34:52 -0300

    Cagada arrumada

--- projeto1/js/js paginas condicionais/condicionalP.js
+++ projeto1/js/js paginas condicionais/condicionalP.js
@@ -18,3 +18,23 @@ function ParabensJ() {
     var txtJ = $("#LExec2").val()
     console.log(txtJ)
 }
+
+function Relatorio() {
+    var checkText = $("#LCheck").val()
+    if (checkText.length < 20) {
+        $("#animetionR").show(1000)
+    } else {
+        alert("Relatorio enviado, ele sera avaliado e uma resposta sera enviada para voce, caso queira entre em contato com nossa equipe")
+        window.location.replace("pagina3.html")
+    }
+}
+
+function Relatorio2() {
+    var checkText = $("#LCheck").val()
+    if (checkText.length < 20) {
+        $("#animetionR").show(1000)
+    } else {
+        alert("Relatorio enviado, ele sera avaliado e uma resposta sera enviada para voce, caso queira entre em contato com nossa equipe")
+        window.location.replace("pagina5.html")
+    }
+}
